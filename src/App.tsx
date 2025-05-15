bdf080c1ab874d73c03fb788bab1d39b5c8c6dc8
Tom Whittle <tomwhittle@hey.com>
2025-05-15 16:28:39 +0100

    updates to splash screen and other bits

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,26 @@ import { StyleSheet } from "react-native";
 import HomeScreen from "./HomeScreen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+  SplashScreen.setOptions({
+    duration: 400,
+    fade: true,
+  });
+
+  useEffect(() => {
+    const prepare = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 100));
+      await SplashScreen.hideAsync();
+    };
+
+    prepare();
+  }, []);
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <StatusBar style="light" translucent={true} />
